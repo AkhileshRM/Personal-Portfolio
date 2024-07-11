b3802e92ece716d7f08e958ff43fc8427dd1fd74
AkhileshRM <70285184+AkhileshRM@users.noreply.github.com>
2024-07-11 13:04:02 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -3,6 +3,7 @@ let hamburger = true
 let nav = document.querySelector(".mobile-container")
 let header = document.querySelector(".header")
 let names = document.querySelector(".name")
+let main = document.querySelector("main")
 
 ham.addEventListener("click", () => {
     if(hamburger === true){
@@ -15,13 +16,14 @@ ham.addEventListener("click", () => {
         ham.style.position="absolute"
         ham.style.top="8%"
         ham.style.right="5%"
-
+        main.style.marginTop="500px"
     }
     else{
         hamburger = true
         ham.classList.add("hamburger")
         ham.classList.remove("close")
         nav.style.display="none";
+        main.style.marginTop="0px"
     }
     console.log(hamburger)
 })
